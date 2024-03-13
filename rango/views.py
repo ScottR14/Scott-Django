6139cfe9a77aa933c9fa3ae7e471d87622d7780a
unknown <2889991R@student.gla.ac.uk>
2024-03-13 13:01:21 +0000

    Added rango picture

--- rango/views.py
+++ rango/views.py
@@ -5,9 +5,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse(
-        "<h1>Rango says hey there partner!</h1>"
-        "<a href='/rango/about/'>About</a>")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
