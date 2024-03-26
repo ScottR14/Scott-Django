34ba018a139185d6374e691e78405d0bf7b61f5e
unknown <2889991R@student.gla.ac.uk>
2024-03-26 19:35:57 +0000

    Updated chapter 7

--- rango/views.py
+++ rango/views.py
@@ -59,21 +59,27 @@ def add_page(request, category_name_slug):
     except Category.DoesNotExist:
         category = None
 
+    # You cannot add a page to a Category that does not exist...
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
+
     form = PageForm()
+
     if request.method == 'POST':
         form = PageForm(request.POST)
+
         if form.is_valid():
             if category:
                 page = form.save(commit=False)
                 page.category = category
                 page.views = 0
                 page.save()
+
                 return redirect(reverse('rango:show_category',
                                         kwargs={'category_name_slug':
-                                                    category_name_slug}))
+                                                category_name_slug}))
         else:
             print(form.errors)
+
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
